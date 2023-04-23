4ce1b41cf17988ec7720fde44427a9a11cce25e2
bdevops99 <bdevops99@gmail.com>
2023-04-23 22:41:32 +0530

    functions

---
+++ functions.sh
@@ -0,0 +1,22 @@
+# A name given to set  of commands called as function
+
+# Declare a function
+
+function_name()
+{
+  echo Hello World
+
+  echo bye
+
+ }
+ # call the function
+ function_name
+ # we can send inputs to the functions and we can access them with special variables $1-$n,$*,$#
+
+ function_name1(){
+   echo first argument = $1
+   echo second argument = $2
+   echo all arguments = $*
+   echo No. of argument = $#
+ }
+ function_name1 123 xyz
