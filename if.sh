7184c954e47c5581b98e78c6d21e7a4653ba308d
bdevops99 <bdevops99@gmail.com>
2023-04-25 21:50:22 +0530

    if conditions

--- if.sh
+++ if.sh
@@ -1,8 +1,11 @@
-fruit_name=mango
-quantity=10
+fruit_name=$1
+quantity=$2
 
-if [ $fruit_name=="mango" ]
+if [ "$fruit_name"=="mango" ]
 then
   echo Mango Quantity - $quantity
+else
+
+  echo fruit does not exist
 
 fi
